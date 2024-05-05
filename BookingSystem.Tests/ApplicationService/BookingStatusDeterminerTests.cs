a80d66a0aea1d62fed7f1a115d04855332da1cf4
Ilija Trnkovski <itr@nebb.net>
2024-05-05 19:00:32 +0200

    added unit tests for status determiner service

---
+++ BookingSystem.Tests/ApplicationService/BookingStatusDeterminerTests.cs
@@ -0,0 +1,72 @@
+﻿using BookingSystem.ApplicationService.Services;
+using BookingSystem.Common.Enums;
+using BookingSystem.Models.ViewModels;
+
+namespace BookingSystem.Tests.ApplicationService
+{
+    [TestClass]
+    public class BookingStatusDeterminerTests
+    {
+        private BookingStatusDeterminer _statusDeterminer;
+
+        [TestInitialize]
+        public void BookingStatusDeterminerTestsInitialize()
+        {
+            _statusDeterminer = new BookingStatusDeterminer();
+        }
+
+        [TestMethod]
+        public void DetermineStatusReturnsPending()
+        {
+            var bookingTime = DateTime.Now;
+            var sleepTime = 60;
+            var bookingRes = new BookRes
+            {
+                BookingTime = bookingTime,
+                SleepTime = sleepTime
+            };
+
+            var result = _statusDeterminer.DetermineStatus(bookingRes);
+
+            Assert.AreEqual(BookingStatusEnum.Pending, result);
+        }
+
+        [TestMethod]
+        public void DetermineStatusReturnsSuccess()
+        {
+            var bookingTime = DateTime.Now.AddMinutes(-2);
+            var sleepTime = 50;
+            var bookingRes = new BookRes
+            {
+                BookingTime = bookingTime,
+                SleepTime = sleepTime,
+                IsHotelBooked = true,
+                IsFlightBooked = true,
+                IsLastMinuteReservation = false
+            };
+
+            var result = _statusDeterminer.DetermineStatus(bookingRes);
+
+            Assert.AreEqual(BookingStatusEnum.Success, result);
+        }
+
+        [TestMethod]
+        public void DetermineStatusReturnsFailed()
+        {
+            var bookingTime = DateTime.Now.AddMinutes(-2);
+            var sleepTime = 35;
+            var bookingRes = new BookRes
+            {
+                BookingTime = bookingTime,
+                SleepTime = sleepTime,
+                IsHotelBooked = true,
+                IsFlightBooked = true,
+                IsLastMinuteReservation = true
+            };
+
+            var result = _statusDeterminer.DetermineStatus(bookingRes);
+
+            Assert.AreEqual(BookingStatusEnum.Failed, result);
+        }
+    }
+}
