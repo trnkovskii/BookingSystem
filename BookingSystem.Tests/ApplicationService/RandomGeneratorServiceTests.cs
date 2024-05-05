64dde70ef0714ec6d911ff2cf4034e51bee73a00
Ilija Trnkovski <itr@nebb.net>
2024-05-05 18:53:06 +0200

    added unit test for random generator service

---
+++ BookingSystem.Tests/ApplicationService/RandomGeneratorServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using BookingSystem.ApplicationService.Interfaces;
+using BookingSystem.ApplicationService.Services;
+
+namespace BookingSystem.Tests.ApplicationService
+{
+    [TestClass]
+    public class RandomGeneratorServiceTests
+    {
+        private RandomGeneratorService _randomGeneratorService;
+
+        [TestInitialize]
+        public void RandomGeneratorServiceTestsInitialize()
+        {
+            _randomGeneratorService = new RandomGeneratorService();
+        }
+
+        [TestMethod]
+        public void GenerateUniqueCodeReturnValid()
+        {
+            string code = _randomGeneratorService.GenerateUniqueCode();
+
+            Assert.IsNotNull(code);
+            Assert.AreEqual(6, code.Length);
+            Assert.IsTrue(code.All(c => char.IsLetterOrDigit(c)));
+        }
+
+        [TestMethod]
+        public void GenerateTimeBetween30and60ReturnValid()
+        {
+            int time = _randomGeneratorService.GenerateRandomTimeBetween30And60();
+
+            Assert.IsTrue(time >= 30 && time < 60);
+        }
+    }
+}
